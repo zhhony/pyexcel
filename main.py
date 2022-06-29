ab171cc72c80198074b3de39ede6ad6d60d650ac
zhhony <zhhony@126.com>
2022-06-29 09:14:27 +0800

    完成新建目录的主体程序

--- main.py
+++ main.py
@@ -3,21 +3,70 @@ from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 import pyApp
 import sys
+import openpyxl
+from pathlib import Path
+from openpyxl.worksheet.hyperlink import Hyperlink
+from openpyxl.styles import Font
+from typing import *
+import threading
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = pyApp.Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    try:
 
-    def docmd():
-        FileName = QFileDialog.getOpenFileName(
-            MainWindow, "选择文件")  # 选择目录，返回选中的路径
-        ui.lineEdit.setText(FileName[0])
-        return FileName
+        font = Font(underline='single', color='FF0000FF')
 
+        app = QApplication(sys.argv)
+        MainWindow = QMainWindow()
+        ui = pyApp.Ui_MainWindow()
+        ui.setupUi(MainWindow)
 
-    ui.openFilesButton.clicked.connect(docmd)
+        def getFileName() -> str:
+            FileName = QFileDialog.getOpenFileName(
+                MainWindow, "选择文件", './', '*.xlsx')  # 选择目录，返回选中的路径
+            ui.lineEdit.setText(FileName[0])
+            return FileName[0]
 
-    MainWindow.show()
-    sys.exit(app.exec())
+        def getSheetName(path: str) -> None:
+            global wb
+            wb = openpyxl.load_workbook(Path(path))
+            wbSheets = [i.title for i in wb]
+            row = 0
+            for i in wbSheets:
+                ui.tableWidget.setItem(row, 0, QTableWidgetItem(i))
+                row += 1
+            return None
+
+        def openFileCMD() -> None:
+            path = getFileName()
+            getSheetName(path)
+            return None
+
+        def commitFileCMD():
+            if '目录' not in [i.title for i in wb]:
+                wb.create_sheet('目录', 0)
+            else:
+                wb['目录'].delete_cols(1, 2)
+            wsList = wb['目录']
+
+            rownum, colnum = 1, 1
+            wsWorkSheetList = [i.title for i in wb if i.title != '目录']
+            wsList.cell(row=rownum, column=colnum, value='目 录')
+            for i in wsWorkSheetList:
+                rownum += 1
+                wsList.cell(row=rownum, column=colnum, value=i).font = font
+                wsList.cell(row=rownum, column=colnum).hyperlink = Hyperlink(
+                    ref='', location='\'%s\'!A1' % i, tooltip=None, display='%s' % i, id=None)
+
+                wb[i]['A3'].font = font
+                wb[i]['A3'].hyperlink = Hyperlink(
+                    ref='', location='\'目录\'!A1', tooltip=None, display='目录', id=None)
+            wb.save()
+            wb.close()
+
+        ui.openFilesButton.clicked.connect(openFileCMD)
+
+        MainWindow.show()
+        sys.exit(app.exec())
+
+    finally:
+        pass
--- pyApp.py
+++ pyApp.py
@@ -15,9 +15,11 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
-    QLabel, QLineEdit, QMainWindow, QPushButton,
-    QSizePolicy, QStackedWidget, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QAbstractItemView, QAbstractScrollArea, QApplication, QFrame,
+    QGridLayout, QHBoxLayout, QHeaderView, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QStackedWidget, QTableWidget, QTableWidgetItem, QVBoxLayout,
+    QWidget)
 import rec_rc
 
 class Ui_MainWindow(object):
@@ -359,24 +361,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
         self.stackedWidget = QStackedWidget(self.RightMidCont)
         self.stackedWidget.setObjectName(u"stackedWidget")
-        self.DirBuid = QWidget()
-        self.DirBuid.setObjectName(u"DirBuid")
-        self.verticalLayout_7 = QVBoxLayout(self.DirBuid)
-        self.verticalLayout_7.setSpacing(0)
-        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
-        self.verticalLayout_7.setContentsMargins(0, 0, 0, 0)
-        self.DirBuidTopTop = QFrame(self.DirBuid)
-        self.DirBuidTopTop.setObjectName(u"DirBuidTopTop")
-        self.DirBuidTopTop.setMaximumSize(QSize(16777215, 50))
-        self.DirBuidTopTop.setFrameShape(QFrame.StyledPanel)
-        self.DirBuidTopTop.setFrameShadow(QFrame.Raised)
-
-        self.verticalLayout_7.addWidget(self.DirBuidTopTop)
-
-        self.DirBuidTop = QFrame(self.DirBuid)
-        self.DirBuidTop.setObjectName(u"DirBuidTop")
-        self.DirBuidTop.setMaximumSize(QSize(16777215, 110))
-        self.DirBuidTop.setStyleSheet(u"QPushButton {\n"
+        self.stackedWidget.setStyleSheet(u"QPushButton {\n"
 "	border: 2px solid rgb(52, 59, 72);\n"
 "	border-radius: 5px;	\n"
 "	background-color: rgb(52, 59, 72);\n"
@@ -389,6 +374,25 @@ class Ui_MainWindow(object):
 "	background-color: rgb(35, 40, 49);\n"
 "	border: 2px solid rgb(43, 50, 61);\n"
 "}")
+        self.DirBuid = QWidget()
+        self.DirBuid.setObjectName(u"DirBuid")
+        self.verticalLayout_7 = QVBoxLayout(self.DirBuid)
+        self.verticalLayout_7.setSpacing(0)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.verticalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.DirBuidTopTop = QFrame(self.DirBuid)
+        self.DirBuidTopTop.setObjectName(u"DirBuidTopTop")
+        self.DirBuidTopTop.setMinimumSize(QSize(0, 50))
+        self.DirBuidTopTop.setMaximumSize(QSize(16777215, 50))
+        self.DirBuidTopTop.setFrameShape(QFrame.StyledPanel)
+        self.DirBuidTopTop.setFrameShadow(QFrame.Raised)
+
+        self.verticalLayout_7.addWidget(self.DirBuidTopTop)
+
+        self.DirBuidTop = QFrame(self.DirBuid)
+        self.DirBuidTop.setObjectName(u"DirBuidTop")
+        self.DirBuidTop.setMaximumSize(QSize(16777215, 110))
+        self.DirBuidTop.setStyleSheet(u"")
         self.DirBuidTop.setFrameShape(QFrame.StyledPanel)
         self.DirBuidTop.setFrameShadow(QFrame.Raised)
         self.gridLayout = QGridLayout(self.DirBuidTop)
@@ -401,7 +405,7 @@ class Ui_MainWindow(object):
         self.lineEdit.setCursor(QCursor(Qt.IBeamCursor))
         self.lineEdit.setStyleSheet(u"#DirBuid .QLineEdit {\n"
 "    font: 8pt \"\u5fae\u8f6f\u96c5\u9ed1\";\n"
-"    color: rgb(100, 100, 100);\n"
+"    color: rgb(170, 170, 170);\n"
 "    background-color: rgb(33, 37, 43);\n"
 "    border-radius: 5px;\n"
 "    border: 2px solid rgb(33, 37, 43);\n"
@@ -451,6 +455,134 @@ class Ui_MainWindow(object):
         self.DirBuidDwn.setObjectName(u"DirBuidDwn")
         self.DirBuidDwn.setFrameShape(QFrame.StyledPanel)
         self.DirBuidDwn.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_8 = QVBoxLayout(self.DirBuidDwn)
+        self.verticalLayout_8.setSpacing(0)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.verticalLayout_8.setContentsMargins(0, 0, 0, 0)
+        self.tableWidget = QTableWidget(self.DirBuidDwn)
+        if (self.tableWidget.columnCount() < 2):
+            self.tableWidget.setColumnCount(2)
+        __qtablewidgetitem = QTableWidgetItem()
+        __qtablewidgetitem.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        __qtablewidgetitem1.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        if (self.tableWidget.rowCount() < 9):
+            self.tableWidget.setRowCount(9)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(0, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(1, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(2, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(3, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(4, __qtablewidgetitem6)
+        __qtablewidgetitem7 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(5, __qtablewidgetitem7)
+        __qtablewidgetitem8 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(6, __qtablewidgetitem8)
+        __qtablewidgetitem9 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(7, __qtablewidgetitem9)
+        __qtablewidgetitem10 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(8, __qtablewidgetitem10)
+        __qtablewidgetitem11 = QTableWidgetItem()
+        self.tableWidget.setItem(0, 0, __qtablewidgetitem11)
+        __qtablewidgetitem12 = QTableWidgetItem()
+        self.tableWidget.setItem(0, 1, __qtablewidgetitem12)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setMaximumSize(QSize(16777215, 300))
+        self.tableWidget.setStyleSheet(u"QTableWidget {	\n"
+"	background-color: transparent;\n"
+"	padding: 10px;\n"
+"	border-radius: 5px;\n"
+"	gridline-color: rgb(44, 49, 58);\n"
+"	border-bottom: 1px solid rgb(44, 49, 60);\n"
+"}\n"
+"QTableWidget::item{\n"
+"	border-color: rgb(44, 49, 60);\n"
+"	padding-left: 5px;\n"
+"	padding-right: 5px;\n"
+"	gridline-color: rgb(44, 49, 60);\n"
+"}\n"
+"QTableWidget::item:selected{\n"
+"	background-color: rgb(189, 147, 249);\n"
+"}\n"
+"QHeaderView::section{\n"
+"	background-color: rgb(33, 37, 43);\n"
+"	max-width: 30px;\n"
+"	border: 1px solid rgb(44, 49, 58);\n"
+"	border-style: none;\n"
+"    border-bottom: 1px solid rgb(44, 49, 60);\n"
+"    border-right: 1px solid rgb(44, 49, 60);\n"
+"}\n"
+"QTableWidget::horizontalHeader {	\n"
+"	background-color: rgb(33, 37, 43);\n"
+"}")
+        self.tableWidget.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.tableWidget.setAutoScroll(False)
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.tableWidget.setDefaultDropAction(Qt.IgnoreAction)
+        self.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.tableWidget.setShowGrid(False)
+        self.tableWidget.setGridStyle(Qt.SolidLine)
+        self.tableWidget.setRowCount(9)
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.horizontalHeader().setVisible(False)
+        self.tableWidget.horizontalHeader().setCascadingSectionResizes(True)
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(200)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.verticalHeader().setStretchLastSection(True)
+
+        self.verticalLayout_8.addWidget(self.tableWidget)
+
+        self.frame = QFrame(self.DirBuidDwn)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(0, 50))
+        self.frame.setMaximumSize(QSize(16777215, 16777215))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_7 = QHBoxLayout(self.frame)
+        self.horizontalLayout_7.setSpacing(0)
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.horizontalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout_7.addWidget(self.frame_2)
+
+        self.frame_4 = QFrame(self.frame)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setMaximumSize(QSize(120, 16777215))
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_8 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_8.setSpacing(6)
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.horizontalLayout_8.setContentsMargins(9, 9, 9, 9)
+        self.commitButton = QPushButton(self.frame_4)
+        self.commitButton.setObjectName(u"commitButton")
+        self.commitButton.setMinimumSize(QSize(100, 30))
+        self.commitButton.setMaximumSize(QSize(150, 30))
+        self.commitButton.setStyleSheet(u"")
+        icon4 = QIcon()
+        icon4.addFile(u":/Icon/images/icons/cil-input.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.commitButton.setIcon(icon4)
+
+        self.horizontalLayout_8.addWidget(self.commitButton)
+
+
+        self.horizontalLayout_7.addWidget(self.frame_4)
+
+
+        self.verticalLayout_8.addWidget(self.frame)
+
 
         self.verticalLayout_7.addWidget(self.DirBuidDwn)
 
@@ -504,7 +636,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\uff1a\u5927\u5c3e\u5df4\u9c7c", None))
-        self.toggleButton.setText(QCoreApplication.translate("MainWindow", u"\u9690\u85cf", None))
+        self.toggleButton.setText(QCoreApplication.translate("MainWindow", u"HOME", None))
         self.btn_home.setText(QCoreApplication.translate("MainWindow", u"\u5efa\u7acb\u76ee\u5f55", None))
         self.btn_widgets.setText(QCoreApplication.translate("MainWindow", u"\u529f\u80fd2", None))
         self.btn_new.setText(QCoreApplication.translate("MainWindow", u"\u529f\u80fd3", None))
@@ -527,6 +659,34 @@ class Ui_MainWindow(object):
         self.labelVersion_4.setText("")
         self.openFilesButton.setText(QCoreApplication.translate("MainWindow", u"\u6253\u5f00", None))
         self.labelBoxBlenderInstalation.setText(QCoreApplication.translate("MainWindow", u"\u6587\u4ef6\u8def\u5f84", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"sheet", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"sheet", None));
+        ___qtablewidgetitem2 = self.tableWidget.verticalHeaderItem(0)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem3 = self.tableWidget.verticalHeaderItem(1)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem4 = self.tableWidget.verticalHeaderItem(2)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem5 = self.tableWidget.verticalHeaderItem(3)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem6 = self.tableWidget.verticalHeaderItem(4)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem7 = self.tableWidget.verticalHeaderItem(5)
+        ___qtablewidgetitem7.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem8 = self.tableWidget.verticalHeaderItem(6)
+        ___qtablewidgetitem8.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem9 = self.tableWidget.verticalHeaderItem(7)
+        ___qtablewidgetitem9.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+        ___qtablewidgetitem10 = self.tableWidget.verticalHeaderItem(8)
+        ___qtablewidgetitem10.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u5efa\u884c", None));
+
+        __sortingEnabled = self.tableWidget.isSortingEnabled()
+        self.tableWidget.setSortingEnabled(False)
+        self.tableWidget.setSortingEnabled(__sortingEnabled)
+
+        self.commitButton.setText(QCoreApplication.translate("MainWindow", u"\u63d0\u4ea4", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u" Product by zhhony@126.com", None))
     # retranslateUi
 
