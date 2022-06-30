dd7f7f2614adc603aa11fd522c1515beaf545548
zhhony <zhhony@126.com>
2022-06-30 10:49:05 +0800

    修正不选择任何文件报错的问题

--- main.py
+++ main.py
@@ -59,38 +59,51 @@ if __name__ == "__main__":
 
         # 定义openFile按钮的动作
         def cmdOpenExcelFile() -> None:
-            excelFilePath = getExcelFilePath(excelFileDir)  # 获取excel路径
-            wbSheetsList = getExcelSheetName(excelFilePath)  # 获取excel的sheet清单
+            try:
+                excelFilePath = getExcelFilePath(excelFileDir)  # 获取excel路径
+                wbSheetsList = getExcelSheetName(
+                    excelFilePath)  # 获取excel的sheet清单
+            except:
+                return None
 
             ui.lineEdit.setText(excelFilePath)  # 将excel路径写入lineEdit
             gridTableWidget(wbSheetsList)  # 将sheet清单写入tableWidget
 
         # 定义commitFileCMD按钮的动作
         def cmdCommitFile() -> None:
-            font = Font(underline='single', color='FF0000FF')
-            excelFilePath = ui.lineEdit.text()  # 读取lineEdit存储的excel路径
-            wb = openpyxl.load_workbook(Path(excelFilePath))  # 打开文件
+            try:
+                font = Font(underline='single', color='FF0000FF')
+                try:
+                    excelFilePath = ui.lineEdit.text()  # 读取lineEdit存储的excel路径
+                    wb = openpyxl.load_workbook(Path(excelFilePath))  # 打开文件
+                except:
+                    return None
 
-            if '目录' not in [i.title for i in wb]:  # 建立空的目录sheet
-                wb.create_sheet('目录', 0)
-            else:
-                wb['目录'].delete_cols(1, 2)
-            wsList = wb['目录']
+                if '目录' not in [i.title for i in wb]:  # 建立空的目录sheet
+                    wb.create_sheet('目录', 0)
+                else:
+                    wb['目录'].delete_cols(1, 2)
+                wsList = wb['目录']
 
-            rownum, colnum = 1, 1
-            wsWorkSheetList = [i.title for i in wb if i.title != '目录']
-            wsList.cell(row=rownum, column=colnum, value='目 录')
-            for i in wsWorkSheetList:
-                rownum += 1
-                wsList.cell(row=rownum, column=colnum, value=i).font = font
-                wsList.cell(row=rownum, column=colnum).hyperlink = Hyperlink(
-                    ref='', location='\'%s\'!A1' % i, tooltip=None, display='%s' % i, id=None)
+                rownum, colnum = 1, 1
+                wsWorkSheetList = [i.title for i in wb if i.title != '目录']
+                wsList.cell(row=rownum, column=colnum, value='目 录')
+                for i in wsWorkSheetList:
+                    rownum += 1
+                    wsList.cell(row=rownum, column=colnum, value=i).font = font
+                    wsList.cell(row=rownum, column=colnum).hyperlink = Hyperlink(
+                        ref='', location='\'%s\'!A1' % i, tooltip=None, display='%s' % i, id=None)
 
-                wb[i]['A3'].font = font
-                wb[i]['A3'].hyperlink = Hyperlink(
-                    ref='', location='\'目录\'!A1', tooltip=None, display='目录', id=None)
-            wb.save(excelFilePath)
-            wb.close()
+                    wb[i]['A3'].font = font
+                    wb[i]['A3'].hyperlink = Hyperlink(
+                        ref='', location='\'目录\'!A1', tooltip=None, display='目录', id=None)
+                wb.save(excelFilePath)
+                QMessageBox.information(MainWindow, '信息', '建立成功！')
+            except:
+                QMessageBox.critical(MainWindow, '错误', '未知错误！')
+            finally:
+                wb.close()
+                return None
 
         MainWindow.show()
 
