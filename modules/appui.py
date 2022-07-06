c8e4aa32939382678e139149e1e88b9bfa609523
zhhony <zhhony@126.com>
2022-07-06 12:05:16 +0800

    更名

--- modules/appui.py
+++ modules/appui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'pyappui.ui'
+## Form generated from reading UI file 'appui.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.3.0
 ##
