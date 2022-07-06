458053064972f9e402b59ea7309899f1449e76df
zhhony <zhhony@126.com>
2022-07-06 13:34:06 +0800

    更新版本号

--- modules/homepage.py
+++ modules/homepage.py
@@ -8,6 +8,6 @@ from py_test_tools import *
 
 class HomePage():
     def __init__(self, MainWindow: QMainWindow, ui: appui.Ui_MainWindow) -> None:
-        self._APP_VERSION = 'v0.4.0'
+        self._APP_VERSION = 'v0.4.1'
         self._MainWindow = MainWindow
         self._ui = ui
